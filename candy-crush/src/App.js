897d7d2a55e0116b632482ee086914a81ed50f13
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-11 17:15:58 +0100

    check for column of four logic

--- candy-crush/src/App.js
+++ candy-crush/src/App.js
@@ -18,6 +18,20 @@ const App = () => {
       }
 		}
 	}
+  
+  
+	const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+
+      if(columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square =>
+          currentColorArrangement[square] = ''
+        )
+      }
+    }
+  }
 
 	const createBoard = () => {
 		const randomColorArrangement = []
@@ -31,10 +45,26 @@ const App = () => {
 		}
 		setCurrentColorArrangement(randomColorArrangement)
 	}
+
+
 	useEffect(() => {
 		createBoard()
 	}, [])
-	console.log(currentColorArrangement)
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+      checkForColumnOfFour()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+  }, [currentColorArrangement, currentColorArrangement, checkForColumnOfFour])
+
+  console.log(currentColorArrangement)
+  
+  
+  
 
 	return (
 		<div className='app'>
