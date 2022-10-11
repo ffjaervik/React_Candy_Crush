6b8930492a3833eef93df6f11da40f9a0477efb5
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-11 17:33:13 +0100

    rowOfThree && rowOfFour functions added

--- candy-crush/src/App.js
+++ candy-crush/src/App.js
@@ -6,32 +6,76 @@ const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 const App = () => {
 	const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
-  const checkForColumnOfFour = () => {
-    for (let i = 0; i < 39; i++) {
-      const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
-      const decidedColor = currentColorArrangement[i]
+	const checkForColumnOfFour = () => {
+		for (let i = 0; i < 39; i++) {
+			const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
+			const decidedColor = currentColorArrangement[i]
 
-      if(columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
-        columnOfFour.forEach(square =>
-          currentColorArrangement[square] = ''
-        )
-      }
-    }
-  }
+			if (
+				columnOfFour.every(
+					(square) => currentColorArrangement[square] === decidedColor
+				)
+			) {
+				columnOfFour.forEach((square) => (currentColorArrangement[square] = ''))
+			}
+		}
+	}
+
+	const checkForRowOfFour = () => {
+		for (let i = 0; i < 64; i++) {
+			const rowOfFour = [i, i + 1, i + 2, i + 3]
+			const decidedColor = currentColorArrangement[i]
+			const notVAlid = [
+				5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+				54, 55, 62, 63, 64,
+			]
+			if (notVAlid.includes(i)) continue
+
+			if (
+				rowOfFour.every(
+					(square) => currentColorArrangement[square] === decidedColor
+				)
+			) {
+				rowOfFour.forEach((square) => (currentColorArrangement[square] = ''))
+			}
+		}
+	}
 
 	const checkForColumnOfThree = () => {
 		for (let i = 0; i < 47; i++) {
 			const columnOfThree = [i, i + width, i + width * 2]
 			const decidedColor = currentColorArrangement[i]
 
-      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
-        columnOfThree.forEach(square => 
-          currentColorArrangement[square] = ''
-        )
-      }
+			if (
+				columnOfThree.every(
+					(square) => currentColorArrangement[square] === decidedColor
+				)
+			) {
+				columnOfThree.forEach(
+					(square) => (currentColorArrangement[square] = '')
+				)
+			}
 		}
 	}
-  
+
+	const checkForRowOfThree = () => {
+		for (let i = 0; i < 64; i++) {
+			const rowOfThree = [i, i + 1, i + 2]
+			const decidedColor = currentColorArrangement[i]
+			const notVAlid = [
+				6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+			]
+			if (notVAlid.includes(i)) continue
+
+			if (
+				rowOfThree.every(
+					(square) => currentColorArrangement[square] === decidedColor
+				)
+			) {
+				rowOfThree.forEach((square) => (currentColorArrangement[square] = ''))
+			}
+		}
+	}
 
 	const createBoard = () => {
 		const randomColorArrangement = []
@@ -46,25 +90,28 @@ const App = () => {
 		setCurrentColorArrangement(randomColorArrangement)
 	}
 
-
 	useEffect(() => {
 		createBoard()
 	}, [])
 
-
-  useEffect(() => {
-    const timer = setInterval(() => {
+	useEffect(() => {
+		const timer = setInterval(() => {
       checkForColumnOfFour()
-      checkForColumnOfThree()
-      setCurrentColorArrangement([...currentColorArrangement])
-    }, 100)
-    return () => clearInterval(timer)
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+      checkForRowOfFour()
+			checkForColumnOfThree()
+			checkForRowOfThree()
+			setCurrentColorArrangement([...currentColorArrangement])
+		}, 100)
+		return () => clearInterval(timer)
+	}, [
+		checkForColumnOfFour,
+    checkForRowOfFour,
+		checkForColumnOfThree,
+		checkForRowOfThree,
+		currentColorArrangement,
+	])
 
-  console.log(currentColorArrangement)
-  
-  
-  
+	console.log(currentColorArrangement)
 
 	return (
 		<div className='app'>
