b7900de6b04966d33adeaa952f6ff834c79e254f
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-11 16:51:02 +0100

    color matching for columns of three

--- candy-crush/src/App.js
+++ candy-crush/src/App.js
@@ -1,29 +1,54 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from 'react'
 
 const width = 8
 const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 
 const App = () => {
-  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+	const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
-  const createBoard = () => {
-    const randomColorArrangement = [];
-    for ( let i = 0; i < width*width; i++){
-      // const randomNumberFrom0To5 = Math.floor(Math.random() * candyColors.length)
-      // const randomColor = candyColors[randomNumberFrom0To5]
-      // Same as the the line bellow
-      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-      randomColorArrangement.push(randomColor)
-    }
-    setCurrentColorArrangement(randomColorArrangement)
-  }
-  useEffect(() => {
-    createBoard()
-  }, [])
-  console.log(currentColorArrangement)
+	const checkForColumnOfThree = () => {
+		for (let i = 0; i < 47; i++) {
+			const columnOfThree = [i, i + width, i + width * 2]
+			const decidedColor = currentColorArrangement[i]
 
+      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => 
+          currentColorArrangement[square] = ''
+        )
+      }
+		}
+	}
 
-	return <div></div>
+	const createBoard = () => {
+		const randomColorArrangement = []
+		for (let i = 0; i < width * width; i++) {
+			// const randomNumberFrom0To5 = Math.floor(Math.random() * candyColors.length)
+			// const randomColor = candyColors[randomNumberFrom0To5]
+			// Same as the the line bellow
+			const randomColor =
+				candyColors[Math.floor(Math.random() * candyColors.length)]
+			randomColorArrangement.push(randomColor)
+		}
+		setCurrentColorArrangement(randomColorArrangement)
+	}
+	useEffect(() => {
+		createBoard()
+	}, [])
+	console.log(currentColorArrangement)
+
+	return (
+		<div className='app'>
+			<div className='game'>
+				{currentColorArrangement.map((candyColor, index) => (
+					<img
+						key={index}
+						style={{ backgroundColor: candyColor }}
+						alt={candyColor}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default App
